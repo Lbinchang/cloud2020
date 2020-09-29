67a4f61cb77413f26618bff61969d8f605450db2
Lbinchang <583994457@qq.com>
2020-09-29 13:55:18 +0800

    openFeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Liangbinchang
+ * @date 2020/9/29  11:24
+ */
+
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
